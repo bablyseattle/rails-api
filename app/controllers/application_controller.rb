2e42aa7029ee2e109134cfa3f8aa42bf95eef8b4
Guest User <Guest@epicodus-39.local>
2017-12-11 15:39:19 -0800

    add ActiveRecord error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::API
 rescue_from ActiveRecord::RecordNotFound do |exception|
   json_response({message: exception.message}, :not_found)
 end
+
+rescue_from ActiveRecord::RecordInvalid do |exception|
+  json_response({message: exception.message}, :validation_failed)
+end
+
 end
